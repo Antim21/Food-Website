879be8366c184ae86410b7473d83d7de7a3f6ca8
Antim Maurya <109455435+Antim21@users.noreply.github.com>
2024-10-20 23:57:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.onscroll = function (){
     if(document.documentElement.scrollTop > 20){
         nav.classList.add("scroll-on")
     }else{
-        nav.classList.remove("scroll-on");
+        nav.classList.remove("scroll-on")
     }
 }
 
